d494beaef0365d6353f297e5e193d90374486b47
keeper0497 <jhonreyartuz01@gmail.com>
2026-03-11 15:18:23 +0800

    Update the migration: add FK in tblunits

--- development/backend/db/migrations/V1.1__unit_table.sql
+++ development/backend/db/migrations/V1.1__unit_table.sql
@@ -6,12 +6,14 @@ CREATE TABLE tblunits (
     name VARCHAR(255) NOT NULL, -- The name of the organizational unit
     sequence_order INT, -- The numerical order of the step in the sequence
     is_active BOOLEAN NOT NULL DEFAULT TRUE, -- 0: InActive, 1: Active
+    org_unit_id INT NOT NULL, -- Foreign key linking to tblorganizational_units.org_unit_id
     created_by INT DEFAULT NULL, -- Audit field for record creator
     updated_by INT DEFAULT NULL, -- Audit field for record updater
     created_at TIMESTAMP NOT NULL DEFAULT NOW(), -- CURRENT_TIMESTAMP ON INSERT
-    updated_at TIMESTAMP NOT NULL DEFAULT NOW() -- CURRENT_TIMESTAMP ON UPDATE
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(), -- CURRENT_TIMESTAMP ON UPDATE
 
     -- Constraints and Relationships
+    FOREIGN KEY (org_unit_id) REFERENCES tblorganizational_units(org_unit_id)
     -- FOREIGN KEY (receiving_officer_id) REFERENCES tblusers(user_id),
     -- FOREIGN KEY (created_by) REFERENCES tblusers(user_id),
     -- FOREIGN KEY (updated_by) REFERENCES tblusers(user_id)
